deed5b9a1d9f7508f1619c7aad39486b62fd8a3e
TojiddinovMuhammadabdullox <rahmatillooyatillo9@gmail.com>
2024-07-16 22:35:11 +0500

    ui changed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,25 +6,31 @@ import '../bloc/currency_state.dart';
 import '../models/currency.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Currency Converter"),
+        title: const Text(
+          "Finance",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        centerTitle: true,
       ),
       body: BlocBuilder<CurrencyBloc, CurrencyState>(
         builder: (context, state) {
           if (state is CurrencyInitial) {
             context.read<CurrencyBloc>().add(FetchCurrencies());
-            return Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator());
           } else if (state is CurrencyLoading) {
-            return Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator());
           } else if (state is CurrencyLoaded) {
             return CurrencyConverter(currencies: state.currencies);
           } else if (state is CurrencyError) {
             return Center(child: Text(state.message));
           } else {
-            return Center(child: Text("Unknown state"));
+            return const Center(child: Text("Unknown state"));
           }
         },
       ),
@@ -35,7 +41,7 @@ class HomeScreen extends StatelessWidget {
 class CurrencyConverter extends StatefulWidget {
   final List<Currency> currencies;
 
-  CurrencyConverter({required this.currencies});
+  const CurrencyConverter({super.key, required this.currencies});
 
   @override
   _CurrencyConverterState createState() => _CurrencyConverterState();
@@ -52,39 +58,75 @@ class _CurrencyConverterState extends State<CurrencyConverter> {
       padding: const EdgeInsets.all(16.0),
       child: Column(
         children: [
-          DropdownButtonFormField<String>(
-            value: _selectedCurrency,
-            hint: Text("Select Currency"),
-            onChanged: (value) {
-              setState(() {
-                _selectedCurrency = value;
-              });
-            },
-            items: widget.currencies
-                .map((currency) => DropdownMenuItem(
-                      value: currency.code,
-                      child: Text(currency.code),
-                    ))
-                .toList(),
+          Row(
+            children: [
+              Expanded(
+                flex: 3,
+                child: TextField(
+                  decoration: const InputDecoration(
+                    labelText: "Miqdorni kiritng",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(12),
+                      ),
+                    ),
+                  ),
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    setState(() {
+                      _inputAmount = double.tryParse(value) ?? 1.0;
+                    });
+                  },
+                ),
+              ),
+              const SizedBox(width: 20),
+              Expanded(
+                flex: 2,
+                child: DropdownButtonFormField<String>(
+                  style: const TextStyle(color: Colors.blue),
+                  value: _selectedCurrency,
+                  hint: const Text("Valyutani tanlang"),
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedCurrency = value;
+                    });
+                  },
+                  items: widget.currencies
+                      .map((currency) => DropdownMenuItem(
+                            value: currency.code,
+                            child: Text(currency.code),
+                          ))
+                      .toList(),
+                ),
+              ),
+            ],
           ),
-          TextField(
-            decoration: InputDecoration(labelText: "Amount"),
-            keyboardType: TextInputType.number,
-            onChanged: (value) {
-              setState(() {
-                _inputAmount = double.tryParse(value) ?? 1.0;
-              });
-            },
-          ),
-          ElevatedButton(
-            onPressed: _convertCurrency,
-            child: Text("Convert"),
-          ),
+          const SizedBox(height: 20),
           if (_convertedAmount != null)
             Text(
-              "Converted Amount: $_convertedAmount UZS",
-              style: TextStyle(fontSize: 20),
+              "${_formatConvertedAmount(_convertedAmount!)} UZS",
+              style: const TextStyle(fontSize: 20, color: Colors.blue),
             ),
+          const SizedBox(height: 50),
+          SizedBox(
+            width: double.infinity,
+            height: 55,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  )),
+              onPressed: _convertCurrency,
+              child: const Text(
+                "Ayirboshlash",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 24),
+              ),
+            ),
+          ),
         ],
       ),
     );
@@ -92,6 +134,9 @@ class _CurrencyConverterState extends State<CurrencyConverter> {
 
   void _convertCurrency() {
     if (_selectedCurrency == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Valyutani tanlang")),
+      );
       return;
     }
 
@@ -102,5 +147,10 @@ class _CurrencyConverterState extends State<CurrencyConverter> {
     setState(() {
       _convertedAmount = _inputAmount * selectedCurrencyRate;
     });
+  }
+
+  String _formatConvertedAmount(double amount) {
+    String amountStr = amount.toStringAsFixed(4);
+    return amountStr;
   }
 }
